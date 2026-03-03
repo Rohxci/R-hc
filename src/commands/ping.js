287aca94468d64da17703baaa4794274ed028f03
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-03 20:16:58 +0100

    Create ping.js

---
+++ src/commands/ping.js
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Check if the bot is online"),
+
+  async execute(interaction) {
+    await interaction.reply("🏓 Pong!");
+  }
+};
