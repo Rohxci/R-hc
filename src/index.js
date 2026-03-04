beb4b7929c191f9bb83c1b00033890a72831bf2c
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-04 12:16:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,6 +56,36 @@ for (const folder of folders) {
 }
 
 /* ========================
+   LOAD EVENTS
+======================== */
+
+const eventsPath = path.join(__dirname, "events");
+
+if (fs.existsSync(eventsPath)) {
+
+ const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter(file => file.endsWith(".js"));
+
+ for (const file of eventFiles) {
+
+  const filePath = path.join(eventsPath, file);
+
+  const event = (await import(`file://${filePath}`)).default;
+
+  if (event.once) {
+   client.once(event.name, (...args) => event.execute(...args));
+  } else {
+   client.on(event.name, (...args) => event.execute(...args));
+  }
+
+  console.log(`Loaded event: ${event.name}`);
+
+ }
+
+}
+
+/* ========================
    REGISTER COMMANDS
 ======================== */
 
