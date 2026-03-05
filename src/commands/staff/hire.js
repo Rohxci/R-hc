b1f533a4c6c9c6c560a127ee2c8801e01b26c0ee
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-06 00:00:28 +0100

    Update hire.js

--- src/commands/staff/hire.js
+++ src/commands/staff/hire.js
@@ -1,31 +1,28 @@
-import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from "discord.js";
-import { getStaffRoles } from "../../utils/staffManager.js";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import fs from "fs";
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName("hire")
-    .setDescription("Hire a user as staff (lowest role)")
-    .addUserOption(o =>
-      o.setName("user").setDescription("User to hire").setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+ data: new SlashCommandBuilder()
+  .setName("hire")
+  .setDescription("Hire a staff member")
+  .addUserOption(o =>
+   o.setName("user").setDescription("User").setRequired(true)
+  )
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
 
-  async execute(interaction) {
-    const member = interaction.options.getMember("user");
-    const roles = getStaffRoles(interaction.guild.id);
+ async execute(interaction) {
 
-    if (!roles)
-      return interaction.reply({ content: "Staff hierarchy not configured.", ephemeral: true });
+  const member = interaction.options.getMember("user");
 
-    const lowestRole = roles[0];
+  const config = JSON.parse(fs.readFileSync("src/data/staffConfig.json"));
 
-    await member.roles.add(lowestRole);
+  const staffRole = interaction.guild.roles.cache.get(config.staffRole);
+  const lowestRole = interaction.guild.roles.cache.get(config.hierarchy[config.hierarchy.length - 1]);
 
-    const embed = new EmbedBuilder()
-      .setColor(0x00ff99)
-      .setTitle("🟢 Staff Hired")
-      .setDescription(`${member.user.tag} is now staff.`)
-      .setTimestamp();
+  await member.roles.add(staffRole);
+  await member.roles.add(lowestRole);
 
-    await interaction.reply({ embeds: [embed] });
-  }
+  await interaction.reply(`${member} hired to staff`);
+
+ }
 };
