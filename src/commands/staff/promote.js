e47c7f748afabbf626a429ba467df5bfccfc890b
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-06 00:08:04 +0100

    Update promote.js

--- src/commands/staff/promote.js
+++ src/commands/staff/promote.js
@@ -1,43 +1,55 @@
-import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from "discord.js";
-import { getStaffRoles, getNextRole } from "../../utils/staffManager.js";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import fs from "fs";
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName("promote")
-    .setDescription("Promote a staff member")
-    .addUserOption(option =>
-      option.setName("user").setDescription("User to promote").setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
-  async execute(interaction) {
-    const member = interaction.options.getMember("user");
-    const roles = getStaffRoles(interaction.guild.id);
+ data: new SlashCommandBuilder()
+  .setName("promote")
+  .setDescription("Promote a staff member")
+  .addUserOption(o =>
+   o.setName("user").setDescription("User").setRequired(true)
+  )
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
 
-    if (!roles)
-      return interaction.reply({ content: "Staff hierarchy not configured.", ephemeral: true });
+ async execute(interaction) {
 
-    const currentRole = roles.find(roleId =>
-      member.roles.cache.has(roleId)
-    );
+  const member = interaction.options.getMember("user");
 
-    if (!currentRole)
-      return interaction.reply({ content: "User is not staff.", ephemeral: true });
+  const config = JSON.parse(
+   fs.readFileSync("src/data/staffConfig.json")
+  );
 
-    const nextRole = getNextRole(interaction.guild.id, currentRole);
-
-    if (!nextRole)
-      return interaction.reply({ content: "User already at highest role.", ephemeral: true });
-
-    await member.roles.remove(currentRole);
-    await member.roles.add(nextRole);
-
-    const embed = new EmbedBuilder()
-      .setColor(0x3399ff)
-      .setTitle("🔼 Staff Promoted")
-      .setDescription(`${member.user.tag} has been promoted.`)
-      .setTimestamp();
-
-    await interaction.reply({ embeds: [embed] });
+  if (!config.hierarchy || config.hierarchy.length === 0) {
+   return interaction.reply("Staff hierarchy not configured.");
   }
+
+  const roles = config.hierarchy;
+
+  let currentIndex = -1;
+
+  for (let i = 0; i < roles.length; i++) {
+   if (member.roles.cache.has(roles[i])) {
+    currentIndex = i;
+    break;
+   }
+  }
+
+  if (currentIndex === -1) {
+   return interaction.reply("User is not staff.");
+  }
+
+  if (currentIndex === 0) {
+   return interaction.reply("User is already highest rank.");
+  }
+
+  const newRole = interaction.guild.roles.cache.get(roles[currentIndex - 1]);
+  const oldRole = interaction.guild.roles.cache.get(roles[currentIndex]);
+
+  await member.roles.remove(oldRole);
+  await member.roles.add(newRole);
+
+  await interaction.reply(`${member} promoted to ${newRole.name}`);
+
+ }
+
 };
